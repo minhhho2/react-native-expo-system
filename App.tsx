1fca4e60f529f40080ffd03221221700d10dbbe9
Minh Nguyen <minhnguyen_96@hotmail.com>
2020-11-02 20:57:09 +1000

    fix imports and screens

--- App.tsx
+++ App.tsx
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import {createAppContainer} from "react-navigation";
+import {Icon} from "expo/build/removed.web";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Whats Up</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const TabNavigator = createBottomTabNavigator(
+	{
+		Home: HomeScreen,
+		Subscription: SubscriptionScreen,
+		Trending: TrendingScreen,
+		Library: LibraryScreen
+	},
+	{
+		defaultNavigationOptions: ({navigation}) => ({
+			tabBarIcon: ({tintColor}) => {
+				const {routeName} = navigation.state;
+				let iconName = getIconName(routeName);
+				// return <Icon name={iconName} size={25} color={tintColor} />
+			}
+		})
+	}
+)
+
+function getIconName(routeName: string) {
+	switch (routeName) {
+		case "Home":
+			return "home";
+		case "Library":
+			return "folder";
+		case "Subscription":
+			return "message-video";
+		case "Trending":
+			return "fire"
+		default:
+			return "";
+	}
+}
+
+function HomeScreen() {
+	return <View>
+		<Text>Home</Text>
+	</View>
+}
+
+function SubscriptionScreen() {
+	return <View>
+		<Text>Subscription</Text>
+	</View>
+}
+
+function TrendingScreen() {
+	return <View>
+		<Text>Trending</Text>
+	</View>
+}
+
+function LibraryScreen() {
+	return <View>
+		<Text>Library</Text>
+	</View>
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#FFFFFF',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	HomeScreen: {
+		backgroundColor: "red",
+		flex: 1,
+	},
+	LibraryScreen: {
+		backgroundColor: "red",
+		flex: 1
+	}
 });
+
+const App = createAppContainer(TabNavigator)
+
+export default App;
