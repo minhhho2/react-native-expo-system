7ec6bbaf9a9d2177cc0d403d42be9985f396c5cf
Minh Nguyen <minhnguyen_96@hotmail.com>
2020-11-15 20:04:00 +1000

    added front and back view state

--- src/components/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -1,5 +1,5 @@
 import {Image, ImageBackground, Text, TouchableOpacity, View} from "react-native";
-import React from "react";
+import React, {useState} from "react";
 import {FeatureCardStyles} from "./FeatureCardStyles";
 import {useNavigation} from "@react-navigation/native";
 import {COLOR, GREY} from "../../constants/ColorVariables";
@@ -15,31 +15,74 @@ export interface IFeatureCardProps {
 	screenUrl: string;
 }
 
+enum FeatureCardViewStateEnum {
+	FRONT,
+	BACK
+}
+
+
 export default function FeatureCard(props: IFeatureCardProps) {
 
-	const navigation = useNavigation();
+	const [viewState, setViewState] = useState<FeatureCardViewStateEnum>(FeatureCardViewStateEnum.FRONT)
 
+	const toggleViewState = () => setViewState(
+		viewState === FeatureCardViewStateEnum.FRONT
+			? FeatureCardViewStateEnum.BACK
+			: FeatureCardViewStateEnum.FRONT
+	)
 	return (
 		<TouchableOpacity
 			style={[FeatureCardStyles.root]}
-			onPress={() => navigation.navigate(props.screenUrl)}
+			onPress={() => toggleViewState()}
 		>
-			<ImageBackground
-				imageStyle={FeatureCardStyles.imageBackground}
-				style={FeatureCardStyles.imageBackground}
-				source={{
-					uri: 'https://wallpapercave.com/wp/wp2149971.jpg',
-				}}
-			>
-				<View style={{marginTop: 'auto'}}>
-					<View style={{backgroundColor: 'rgba(30, 30, 30, 0.6)', borderBottomLeftRadius: 10, borderBottomRightRadius: 10}}>
-						<Text style={{color: COLOR.WHITE, padding: 20}}>{props.name}</Text>
-					</View>
-				</View>
-			</ImageBackground>
-
-
+			{viewState === FeatureCardViewStateEnum.FRONT && FrontView(props)}
+			{viewState === FeatureCardViewStateEnum.BACK && BackView(props)}
 		</TouchableOpacity>
 
+	)
+}
+
+function FrontView(props: IFeatureCardProps) {
+	const navigation = useNavigation();
+
+	return (
+		<ImageBackground
+			imageStyle={FeatureCardStyles.imageBackground}
+			style={FeatureCardStyles.imageBackground}
+			source={{
+				uri: 'https://wallpapercave.com/wp/wp2149971.jpg',
+			}}
+		>
+			<View style={{marginTop: 'auto'}}>
+				<TouchableOpacity
+					onPress={() => navigation.navigate(props.screenUrl)}
+				>
+
+					<View style={{
+						backgroundColor: 'rgba(30, 30, 30, 0.6)',
+						borderBottomLeftRadius: 10,
+						borderBottomRightRadius: 10
+					}}>
+						<Text style={{color: COLOR.WHITE, padding: 20}}>{props.name}</Text>
+					</View>
+				</TouchableOpacity>
+
+			</View>
+		</ImageBackground>
+	)
+}
+
+function BackView(props: IFeatureCardProps) {
+	return (
+		<ImageBackground
+			imageStyle={FeatureCardStyles.imageBackground}
+			style={FeatureCardStyles.imageBackground}
+			source={{
+				uri: 'https://www.google.com/search?q=abstract+light+background&sxsrf=ALeKk00-6RJZyWLJTf3nGAlLZCJ8jPZFUQ:1605434198715&tbm=isch&source=iu&ictx=1&fir=yhArlbWZAqVXuM%252C1sTQfkzawziIWM%252C_&vet=1&usg=AI4_-kQKhvfTK-pupNsAyTtVMhl12tCyyw&sa=X&ved=2ahUKEwiLov-4pITtAhVcxzgGHUT5DvAQ9QF6BAgLEGY&biw=1018&bih=1084#imgrc=yhArlbWZAqVXuM&imgdii=d0d0llSS9IeixM'
+			}}
+		>
+			<Text style={{color: COLOR.WHITE, padding: 20}}>{props.description}</Text>
+			<Text style={{color: COLOR.WHITE, padding: 20}}>{props.screenUrl}</Text>
+		</ImageBackground>
 	)
 }
